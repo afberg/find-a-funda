96288fb07148f7ef505412aa8eff6f7cbd76889e
Anton Forsberg <a.forsberg.91@gmail.com>
2019-06-06 17:17:18 +0200

    Generates webpack boilerplate

---
+++ webpack.config.js
@@ -0,0 +1,76 @@
+const path = require("path");
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const webpack = require('webpack');
+module.exports = env => {
+    const isProd = env.production === "true";
+    console.log('NODE_ENV: ', env.NODE_ENV);
+    console.log('Production: ',isProd);
+    return {
+        mode: isProd ? "production": "development",
+        devtool: isProd ? "" :"inline-source-map",
+        watch: !isProd, 
+        entry: {
+            app: './src/app.js',
+        },
+        output: {
+            filename: '[name].bundle.js',
+            path: path.resolve(__dirname, './dist/')
+        },
+        resolve: {
+            extensions: [".ts", ".js"]
+        },
+        devServer: {
+            contentBase: path.join(__dirname, 'dist'),
+            hot: true
+        },
+        module: {
+            rules: [{
+                test: /\.css$/,
+                use: [
+                    {
+                        loader: 'style-loader',
+                        options: {
+                            hmr: true,
+                        },
+                    },
+                    {
+                        loader: 'css-loader',
+                        options: {
+                            importLoaders: 1
+                        }
+                    },
+                    {
+                        loader: 'postcss-loader'
+                    }
+                ]
+            },
+            {
+                test: /\.js$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: 'babel-loader',
+                    options: {
+                        presets: ['@babel/preset-env']
+                    }
+                }
+            },
+            {
+                test: /\.ts$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: 'ts-loader'
+                }
+            }
+        ],
+            
+        },
+        plugins: [
+            new CleanWebpackPlugin(),
+            new HtmlWebpackPlugin({
+                template: './src/index.html'
+            }),
+            new webpack.HotModuleReplacementPlugin()
+        ],
+    }
+};
